10d5fd60c2cc49d050b5626fa63912c1f1bbd1da
Omer Hanan <omer.hanan@gmail.com>
2022-06-14 21:42:43 +0300

    Update program file

--- Program_1.cs
+++ Program_1.cs
@@ -11,29 +11,84 @@ namespace ShareableSpreadSheet
     {
         public static void Main(string[] args)
         {
-            int Rows = 100;
-            int Columns = 100;
-            int numOfThreads = 30;
-            int milis = 500;
-            SharableSpreadSheet spreadSheet = new SharableSpreadSheet(Rows, Columns);
+            if (args.Length != 5)
+            {
+                throw new Exception("need to enter 5 arguments");
+            }
 
-            Simulator();
+            int rows = 0;
+            try
+            {
+                rows = int.Parse(args[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("First argument is not a number!");
+                Console.WriteLine(ex.Message);
+            }
 
-            void Simulator()
+            int cols = 0;
+            try
+            {
+                cols = int.Parse(args[1]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Second argument is not a number!");
+                Console.WriteLine(ex.Message);
+            }
+
+            int nThreads = 0;
+            try
+            {
+                nThreads = int.Parse(args[2]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Third argument is not a number!");
+                Console.WriteLine(ex.Message);
+            }
+
+            int nOperations = 0;
+            try
+            {
+                nOperations = int.Parse(args[3]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("4th argument is not a number!");
+                Console.WriteLine(ex.Message);
+            }
+
+            int mssleep = 0;
+            try
+            {
+                mssleep = int.Parse(args[4]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("5th argument is not a number!");
+                Console.WriteLine(ex.Message);
+            }
+
+            SharableSpreadSheet spreadSheet = new SharableSpreadSheet(rows, cols);
+            Simulator(rows, cols, nThreads, nOperations, mssleep);
+
+            void Simulator(int rows, int cols, int nThreads, int nOperations, int mssleep)
             {
                 int count = 1;
-          
-                for (int i = 0; i < Rows; i++)
+
+                for (int i = 0; i < rows; i++)
                 {
-                    for (int j = 0; j < Columns; j++)
+                    for (int j = 0; j < cols; j++)
                     {
-                        spreadSheet.setCell(i,j,$"({i},{j})");
+                        spreadSheet.setCell(i, j, $"({i},{j})");
                         count++;
                     }
                 }
 
                 List<Thread> threads = new List<Thread>();
-                for (int i = 0; i < numOfThreads; i++)
+                for (int i = 0; i < nThreads; i++)
                 {
                     var th = new Thread(DoWork);
                     threads.Add(th);
@@ -47,7 +102,7 @@ namespace ShareableSpreadSheet
 
             void DoWork()
             {
-               
+
                 Console.Write("User[" + Thread.CurrentThread.ManagedThreadId + "]:");
                 for (int i = 0; i < 14; i++)
                 {
@@ -64,6 +119,7 @@ namespace ShareableSpreadSheet
                         case 1:
                             spreadSheet.setCell(0, 0, "Eden");
                             Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "]" + " string 'Eden' inserted to cell [0,0]");
+
 
                             break;
                         case 2:
@@ -83,20 +139,20 @@ namespace ShareableSpreadSheet
                             break;
                         case 5:
                             Random rnd1 = new Random();
-                            int c = rnd.Next(0, Columns);
-                            //string s4 = spreadSheet.getCell(0, c);
+                            int c = rnd.Next(0, cols);
+                            string s4 = spreadSheet.getCell(0, c);
 
-                            int Row = spreadSheet.searchInRow(0, "testcell0");
-                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] string " + "testsell0" + " was found in Row " + Row.ToString());
+                            int Row = spreadSheet.searchInRow(0, s4);
+                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] string " + s4 + " was found in Row " + Row.ToString());
 
                             break;
                         case 6:
                             Random rnd2 = new Random();
-                            //int row = rnd.Next(0, rows);
-                            //string s5 = spreadSheet.getCell(0, row);
+                            int row = rnd.Next(0, rows);
+                            string s5 = spreadSheet.getCell(0, row);
 
-                            int Col = spreadSheet.searchInCol(0, "testcell61");
-                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] string" + "testcell61" + " was found in Col " + Col.ToString());
+                            int Col = spreadSheet.searchInCol(0, s5);
+                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] string " + s5 + " was found in Col " + Col.ToString());
 
                             break;
                         case 7:
@@ -106,39 +162,39 @@ namespace ShareableSpreadSheet
                             break;
                         case 8:
                             spreadSheet.addRow(2);
-                            Rows++;
+                            rows++;
 
-                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + "add another row after row 0");
+                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + "add another row after row 2");
                             break;
                         case 9:
                             spreadSheet.addCol(0);
-                            Columns++;
+                            cols++;
 
                             Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + "add another colum after column 0");
 
                             break;
                         case 10:
-                            //string s7 = spreadSheet.getCell(0, 2);
-                            spreadSheet.findAll("testcell99", true);
-                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + "the string " + "testcell99" + " was found in cell (0,2)");
+                            string s7 = spreadSheet.getCell(0, 2);
+                            spreadSheet.findAll(s7, true);
+                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + "the string " + s7 + " was found in cell (0,2)");
                             break;
                         case 11:
-                            //string s8 = spreadSheet.getCell(0, 3);
-                            spreadSheet.setAll("testcell65", "omer", true);
-                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + "the string " + "testcell65" + " was replaced with the string 'omer'");
+                            string s8 = spreadSheet.getCell(0, 3);
+                            spreadSheet.setAll(s8, "omer", true);
+                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] " + "the string " + s8 + " was replaced with the string 'omer'");
                             break;
                         case 12:
                             spreadSheet.getSize();
 
-                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] the current size of columns:" + Columns.ToString() + " and current size of rows:" + Rows.ToString());
+                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] the current size of columns:" + cols.ToString() + " and current size of rows:" + rows.ToString());
                             break;
                         case 13:
-                            spreadSheet.setConcurrentSearchLimit(numOfThreads);
-                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] update the current size to " + numOfThreads.ToString());
+                            spreadSheet.setConcurrentSearchLimit(nThreads);
+                            Console.WriteLine("User[" + Thread.CurrentThread.ManagedThreadId + "]:" + "[" + DateTime.Now.ToString("HH:mm:ss tt") + "] update the current size to " + nThreads.ToString());
                             break;
 
                     }
-                    Thread.Sleep(milis);
+                    Thread.Sleep(mssleep);
 
 
                 }
